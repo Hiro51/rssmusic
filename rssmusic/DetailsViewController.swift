dde03ac499471e6820bc8cc667f2220ec698895f
Hiro Tsujino <h.tsujino0714@gmail.com>
2014-11-10 00:19:00 +0900

    Creating a second view.

---
+++ rssmusic/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  rssmusic
+//
+//  Created by Hiroyuki Tsujino on 11/9/14.
+//  Copyright (c) 2014 Hiroyuki Tsujino. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    var album: Album?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
